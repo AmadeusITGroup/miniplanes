fcc0712805b3f5fdfc921b3ea1098d12bc04a1dc
Salvatore Dario Minonne <salvatore-dario.minonne@amadeus.com>
2019-02-11 11:22:50 +0100

    removing Apache license

--- storage/pkg/db/db.go
+++ storage/pkg/db/db.go
@@ -1,14 +1,1 @@
-package db
-
-import "github.com/amadeusitgroup/miniapp/storage/pkg/gen/models"
-
-// DB defines db interface
-type DB interface {
-	Ping() error
-	DialString() string
-	GetSchedules() ([]*models.Schedule, error)
-	GetAirlines() ([]*models.Airline, error)
-	GetCourses() ([]*models.Course, error)
-	GetAirports() ([]*models.Airport, error)
-	InsertSchedule(s *models.Schedule) error
 }
